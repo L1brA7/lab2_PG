39426c10b177304ea581d83046e2240c2c5ec618
LibrA <umashev58@gnail.com>
2020-03-06 14:27:21 +0300

    Перевел кое-что в функции

--- lab2_PG/lab2_PG.cpp
+++ lab2_PG/lab2_PG.cpp
@@ -1,11 +1,13 @@
 ﻿#include <iostream>
-#include <cstring>
+#include <tuple>
 using namespace std;
 
-void field_creator(int** A, int N, int M);
-void after_field(int **A, int N, int M);
+void num_changer(int *N);
+tuple<int, int> WH();
+int **memory_allocator(int N, int M);
+void field_creator(int **A, int N, int M);
 void output_2D(int **A, int N, int M);
-int scan(int** field, int x, int y);
+int scan(int** field, int N, int M, int x, int y);
 void darvin_process(int **field, int **afterfield, int N, int M);
 
 int main() {
@@ -13,25 +15,40 @@ int main() {
 	cout << "Enter the number, 0 to close - "; cin >> number;
 	while (number) {
 		if (number == 1) {
-			int N, M;
-			cout << "Enter field's width - "; cin >> N; //вводим ширину поля
-			cout << "Enter field's height - "; cin >> M; //вводим высоту поля
-
-			int **field = new int *[N];     
-			int **afterfield = new int *[N];
-			for (int i = 0; i < M; i++) {
-				field[i] = new int[M];
-				afterfield[i] = new int[M];
-			}
-
+			int N, M; tie(N, M) = WH();
+			int **field = memory_allocator(N, M);
+			int **afterfield = memory_allocator(N, M);
 			field_creator(field, N, M);
 			output_2D(field, N, M);
-			cout << scan(field, 1, 1) << endl;
-
-			cout << "Enter the number, 0 to close - " << endl; cin >> number;
+			cout << scan(field, N, M, 1, 1) << endl;
+			num_changer(&number);
 		}
 	}
 	return 0;
+}
+
+/*
+ввод ширины N и высоты M
+первое значение - ширина, второе -  высота
+*/
+tuple<int, int> WH() {
+	int N, M;
+	cout << "Enter array's width - "; cin >> N;
+	cout << "Enter array's height - "; cin >> M;
+	return make_tuple(N, M);
+}
+
+//выделение памяти по заданной ширине и высоте
+int** memory_allocator(int N, int M) {
+	int **A = new int *[N];
+	for (int i = 0; i < M; i++) A[i] = new int[M];
+	return A;
+}
+
+//смена номера задания
+void num_changer(int* N) {
+	cout << "Enter the number, 0 to close - " << endl;
+	cin >> *N;
 }
 
 //создание поля брани для "Игра жизнь"
@@ -48,7 +65,7 @@ void output_2D(int** A, int N, int M) {
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < M; j++) {
 			printf("%i ", A[i][j]);
-			if(j == N - 1) cout << endl;
+			if (j == N - 1) cout << endl;
 		}
 	}
 }
@@ -59,17 +76,23 @@ void darvin_process(int **field, int **afterfield, int N, int M) {
 }
 
 /*
-осмотр соседей клетки возвращает количество живых клеток вокруг проверяемой
-если проверяется живая клетка, выводит количество - 1, т.к. саму клетку мы не считаем
+осмотр соседей клетки: возвращает количество живых клеток вокруг проверяемой
+если проверяется живая клетка, выводит (количество - 1), т.к. саму клетку мы не считаем
 если мертвая, то она не увеличивает количество, пожтому просто выводим его
 */
-int scan(int **field, int x, int y) {
+int scan(int **field, int N, int M, int x, int y) {
 	int quantity = 0;
-	for(int i = x - 1; i <= x + 1; i++){
-		for(int j = y - 1; j <= y + 1; j++){
-			if(field[i][j] == 1) quantity++;
+
+	//цикл для "внутреполевых" клеток
+	if (x > 0 && y > 0 && x < N - 1 && y < M - 1) {
+		for(int i = x - 1; i <= x + 1; i++){
+			for(int j = y - 1; j <= y + 1; j++){
+				if (field[i][j] == 1) quantity++;
+			}
 		}
 	}
-	if(field[x][y] == 1) return quantity - 1;
+
+
+	if (field[x][y] == 1) return quantity - 1;
 	else return quantity;
 }
