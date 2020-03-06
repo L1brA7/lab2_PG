06b45d750f90626df0fa52b7d9fb381a98f2a886
LibrA <umashev58@gnail.com>
2020-03-06 12:25:44 +0300

    Поле брани готово

---
+++ lab2_PG/lab2_PG.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <cstring>
+using namespace std;
+
+void field_creator(int** A, int N, int M);
+void A_output(int **A, int N, int M);
+
+int main() {
+	int N, M; cout << "Enter field's width - "; cin >> N; //вводим ширину поля
+	cout << "Enter field's height - "; cin >> M; //вводим высоту поля
+	int** field = new int* [N];
+	for (int i = 0; i < M; i++) field[i] = new int[M];
+	field_creator(field, N, M);
+	A_output(field, N, M);
+	return 0;
+}
+
+//создание поля брани для "Игра жизнь"
+void field_creator(int** A, int N, int M) {
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			A[i][j] = rand() % 2;
+		}
+	}
+}
+
+//вывод для двумерного массива N - строки, M - столбцы
+void A_output(int** A, int N, int M) {
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			printf("%i\t", A[i][j]);
+			if(j == N - 1) cout << endl;
+		}
+	}
+}
