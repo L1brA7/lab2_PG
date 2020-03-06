8dd442685ae40127be7b5ea72a1da141e665ae4e
LibrA <umashev58@gnail.com>
2020-03-06 18:00:56 +0300

    -ошибка вывода не квадратных полей и улучшен вывод

--- lab2_PG/lab2_PG.cpp
+++ lab2_PG/lab2_PG.cpp
@@ -29,7 +29,7 @@ void darvin_process(int **field, int **afterfield, int N, int M);
 если проверяется живая клетка, выводит (количество - 1), т.к. саму клетку мы не считаем
 если мертвая, то она не увеличивает количество, пожтому просто выводим его
 */
-int scan(int** field, int N, int M, int x, int y);
+int scan(int** field, int x, int y);
 
 int main() {
 
@@ -37,7 +37,7 @@ int main() {
 	num_changer(&number);
 	while (number) {
 		if (number == 1) {
-			int N, M; tie(N, M) = WH(); N+=2, M=N; //увеличиваем значения на 2 для создания мертвой зоны, которая потом не заполняется и не выводится
+			int N, M; tie(N, M) = WH(); N+=2, M+=2; //увеличиваем значение на 2 для создания мертвой зоны, которая потом не заполняется и не выводится
 			int **field = memory_allocator(N, M);
 			int **afterfield = memory_allocator(N, M);
 			field_creator(field, N, M);
@@ -96,22 +96,20 @@ void field_creator(int** A, int N, int M) {
 
 void field_out(int **A, int N, int M) {
 	for (int i = 1; i < N - 1; i++) {
-		for (int j = 1; j < M - 1; j++) {
-			printf("%i ", A[i][j]);
-			if (j == N - 2) cout << endl;
-		}
+		for (int j = 1; j < M - 1; j++) printf("%i ", A[i][j]);
+		printf("\n");
 	}
 	printf("\n");
 }
 
 void darvin_process(int **field, int **afterfield, int N, int M) {
-	int lifes, cycles, add;
+	int lifes, cycles, tick = 0;
 	cout << "Enter the quantity of life cycles - "; cin >> cycles;
 	N--, M--;
 	while (cycles) {
 		for (int i = 1; i < N; i++) {
 			for (int j = 1; j < M; j++) {
-				lifes = scan(field, N, M, i, j);
+				lifes = scan(field, i, j);
 				if (field[i][j] == 1) {
 					if (lifes == 2 || lifes == 3) afterfield[i][j] = 1;
 					else afterfield[i][j] = 0;
@@ -121,13 +119,16 @@ void darvin_process(int **field, int **afterfield, int N, int M) {
 					else afterfield[i][j] = 0;
 			}
 		}
-		cycles--;
-		memcpy(afterfield, field, sizeof(int) * (N+1) * (N+1));
+		cycles--; tick++;
+		cout << "field - " << tick << endl;
+		field_out(field, N + 1, M + 1);
+		cout << "afterfield - " << tick << endl;
 		field_out(afterfield, N + 1, M + 1);
+		memcpy(afterfield, field, sizeof(int) * (N + 1) * (N + 1));
 	}
 }
 
-int scan(int **field, int N, int M, int x, int y) {
+int scan(int **field, int x, int y) {
 	int quantity = 0;
 	for(int i = x - 1; i <= x + 1; i++){
 		for(int j = y - 1; j <= y + 1; j++){
