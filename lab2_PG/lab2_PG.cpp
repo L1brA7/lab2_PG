6639b22d2fea11610c223290cc98ac304aa66ea1
LibrA <umashev58@gnail.com>
2020-03-07 08:44:52 +0300

    Начинаю делать задание 3

--- lab2_PG/lab2_PG.cpp
+++ lab2_PG/lab2_PG.cpp
@@ -1,5 +1,6 @@
 ﻿#include <iostream>
 #include <tuple>
+#include <string>
 using namespace std;
 
 //функции общего назначения
@@ -8,8 +9,8 @@ using namespace std;
 ввод ширины N и высоты M
 первое значение - ширина, второе -  высота
 */
-tuple<int, int> WH();
-//выделение памяти по заданной ширине и высоте
+tuple<int, int> WH(int number);
+//выделение памяти по заданной ширине и высоте (number влияет только на вывод)
 int** memory_allocator(int N, int M);
 //смена номера задания
 void num_changer(int* N);
@@ -31,36 +32,54 @@ void darvin_process(int **field, int **afterfield, int N, int M);
 */
 int scan(int** field, int x, int y);
 
-int main() {
+//функции для задания 2
 
+//функция заполняющая табло очков результатами выстрелов
+void shots(int **scoreboard, int N, int M);
+//подсчет результатов
+void res_calc(int **scoreboard, int N, int M);
+
+int main() {
 	int number;
 	num_changer(&number);
 	while (number) {
 		if (number == 1) {
-			int N, M; tie(N, M) = WH(); N+=2, M+=2;
+			int N, M; tie(N, M) = WH(number); N+=2, M+=2;
 			int **field = memory_allocator(N, M);
 			int **afterfield = memory_allocator(N, M);
 			field_creator(field, N, M);
 			darvin_process(field, afterfield, N, M);
 			delete[] field, afterfield;
 		}
+		if (number == 2) {}
+		if (number == 3) {
+			int N, M; tie(N, M) = WH(number);
+			int **scoreboard = memory_allocator(N, M);
+			//int **results = memory_allocator(3, M); //доска результатов, ширина всегда 3
+			shots(scoreboard, N, M);
+			output_2D(scoreboard, N, M);
+		}
 		num_changer(&number);
 	}
-
 	return 0;
 }
 
-tuple<int, int> WH() {
+tuple<int, int> WH(int number) {
 	int N, M;
-	cout << "Enter the array's height - "; cin >> N;
-	while (N < 0) {
-		cout << "ERROR. Enter the positive array's height - ";
-		cin >> N;
-	}
-	cout << "Enter the array's width - "; cin >> M;
+	string arr_type;
+	if (number == 1) arr_type = "field";
+	if (number == 2) arr_type = "matrix";
+	if (number == 3) arr_type = "scorboard";
+	cout << "Enter the " + arr_type + "'s width - "; cin >> M;
 	while (M < 0) {
-		cout << "ERROR. Enter the positive array's width - ";
+		cout << "ERROR. Enter the positive " + arr_type + "'s width - ";
 		cin >> M;
+	}
+	cout << "Enter the " + arr_type + "'s height - ";
+	cin >> N;
+	while (N < 0) {
+		cout << "ERROR. Enter the positive " + arr_type + "'s height - ";
+		cin >> N;
 	}
 	printf("\n");
 	return make_tuple(N, M);
@@ -84,17 +103,17 @@ void output_2D(int** A, int N, int M) {
 	}
 }
 
-void field_creator(int** A, int N, int M) {
+void field_creator(int** field, int N, int M) {
 	for (int i = 1; i < N - 1; i++) {
 		for (int j = 1; j < M - 1; j++) {
-			A[i][j] = rand() % 2;
+			field[i][j] = rand() % 2;
 		}
 	}
 }
 
-void field_out(int **A, int N, int M) {
+void field_out(int **field, int N, int M) {
 	for (int i = 1; i < N - 1; i++) {
-		for (int j = 1; j < M - 1; j++) printf("%i ", A[i][j]);
+		for (int j = 1; j < M - 1; j++) printf("%i ", field[i][j]);
 		printf("\n");
 	}
 	printf("\n");
@@ -138,4 +157,16 @@ int scan(int **field, int x, int y) {
 	}
 	if (field[x][y] == 1) return quantity - 1;
 	else return quantity;
+}
+
+void shots(int **scoreboard, int N, int M) {
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			scoreboard[i][j] = rand() % 11;
+		}
+	}
+}
+
+void res_calc(int **scoreboard, int N, int M) {
+	
 }
